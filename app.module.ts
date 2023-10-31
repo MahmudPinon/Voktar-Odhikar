6f987b48d86c22bf1716c1511164ae62d77e0375
Rasel Mahmud <108051691+MahmudPinon@users.noreply.github.com>
2023-10-31 23:31:35 +0600

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -1,20 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { IndustryModule } from './Users/Industry/industry.module';
-
+import { AdminModule } from './Users/Admin/admin.module';
 @Module({
-    imports: [IndustryModule, TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: '1535kazi',
-        database: 'Vhoktar Odhikar',//Change to your database name
-        autoLoadEntities: true,
-        synchronize: true,
-    } ),
-    ],
-    controllers: [],
-    providers: [],
+imports: [AdminModule, TypeOrmModule.forRoot(
+{ type: 'postgres',
+host: 'localhost',
+port: 5432,
+username: 'postgres',
+password: '20438672',
+database: 'Vhoktar Odhikar',//Change to your database name
+autoLoadEntities: true,
+synchronize: true,
+} ),
+],
+controllers: [],
+providers: [],
 })
 export class AppModule {}
